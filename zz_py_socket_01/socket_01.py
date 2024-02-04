7d425753c61a44635a7925d5e664b4dcb230c9a2
YR <darkbuq@gmail.com>
2024-02-04 21:18:33 +0800

    Update socket_01.py

--- zz_py_socket_01/socket_01.py
+++ zz_py_socket_01/socket_01.py
@@ -4,9 +4,15 @@ TCP_IP      = '169.254.37.237'  # or whatever address you'll find on the E4980A 
 TCP_PORT    = 5025
 BUFFER_SIZE = 1024
 
+
+#%%
+
 def query(cmd):
     s.send(bytes(cmd+"\r\n",'ansi'))
     return s.recv(BUFFER_SIZE).decode('utf8')
+
+
+#%%
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.settimeout(3.0)
